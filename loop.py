dbcee8b11b7d19e1e3daaacc1aff0d3b9c78cf92
Ikeh Victor <victorikeh77@gmail.com>
2024-03-13 10:59:02 -0400

    investment calculator

--- loop.py
+++ loop.py
@@ -9,7 +9,7 @@ money = float(money)
 
 interest_rate = float(interest_rate) * .01
 
-for i in range(10):
+for _i in range(10):
     money = money + (money * interest_rate)
 
 print("Investment after 10 years : {:.2f}".format(money))
